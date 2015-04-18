c3b934bdb8a541d4048339b677bdff2297e378d9
developer <developer@developer-desktop>
2015-04-18 18:24:13 +0200

    ComponentHelper-test added

---
+++ src/test/java/com/itelg/zkoss/helper/component/ComponentHelperTest.java
@@ -0,0 +1,23 @@
+package com.itelg.zkoss.helper.component;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
+
+public class ComponentHelperTest
+{
+	@Test
+	public void testRemoveAllChildren()
+	{
+		Listbox listbox = new Listbox();
+		Assert.assertEquals(0, listbox.getChildren().size());
+
+		Listitem listitem = new Listitem();
+		listbox.appendChild(listitem);
+		Assert.assertEquals(1, listbox.getChildren().size());
+		
+		ComponentHelper.removeAllChildren(listbox);
+		Assert.assertEquals(0, listbox.getChildren().size());
+	}
+}
